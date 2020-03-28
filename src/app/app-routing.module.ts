1ff69e58c58d44225e52ed890020241a1f7a2947
nyazkhan <khannyaz1705@gmail.com>
2020-03-28 13:41:23 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
 
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+
+  {
+    path: 'map',
+    loadChildren: () => import('./module/map/map.module').then(m => m.MapModule)
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
